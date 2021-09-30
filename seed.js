1524554fa51ed25f958dc0e8982fffed662d2059
Brian Kasprzyk <bkasprzyk@outlook.com>
2021-09-30 14:44:06 -0700

    atlas working

--- seed.js
+++ seed.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/books');
-const Book = require('./models/book.js')
+mongoose.connect(process.env.MONGODB_URI);
+const Book = require('./models/book.js');
 
 async function seed(){
-  const anthonyBook = new Book({
+  const anthonyBook = await new Book({
     title: 'Jurrasic Park',
     description: 'A cautionary tale about genetic engineering, it presents the collapse of an amusement park showcasing genetically re-created dinosaurs to illustrate the mathematical concept of chaos theory and its real-world implications.',
     status: true,
     email: 'anthonymorton760@gmail.com'
 
   });
-  const brianBook = new Book({
+  const brianBook = await new Book({
     title: 'Redwall',
     description: 'Originally published in 1986, it is the first book of the Redwall series.',
     status: true,
     email: 'bkasprzyk@outlook.com'
 
   });
-  const harveyBook = new Book({
+  const harveyBook = await new Book({
     title: 'Odyssey',
     description: 'The Odyssey is one of two major ancient Greek epic poems attributed to Homer',
     status: true,
@@ -33,6 +33,6 @@ async function seed(){
   brianBook.save();
   harveyBook.save();
   mongoose.disconnect();
-}
+};
 
 seed();
