44206495e9da58be578907dc1ad9946369c24be3
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-10-02 09:06:35 -0700

    new branch commit

--- server.js
+++ server.js
@@ -9,40 +9,6 @@ app.use(cors());
 const Book = require("./models/book");
 mongoose.connect(process.env.MONGODB_URI);
 app.use(express.json());
-
-// const anthonyBook = new Book({
-//   title: "Jurrasic Park",
-//   description:
-//     "A cautionary tale about genetic engineering, it presents the collapse of an amusement park showcasing genetically re-created dinosaurs to illustrate the mathematical concept of chaos theory and its real-world implications.",
-//   status: true,
-//   email: "anthonymorton760@gmail.com",
-// });
-// const brianBook = new Book({
-//   title: "Redwall",
-//   description:
-//     "Originally published in 1986, it is the first book of the Redwall series.",
-//   status: true,
-//   email: "bkasprzyk@outlook.com",
-// });
-// const harveyBook = new Book({
-//   title: "Odyssey",
-//   description:
-//     "The Odyssey is one of two major ancient Greek epic poems attributed to Homer",
-//   status: true,
-//   email: "tripppdx@gmail.com",
-// });
-// anthonyBook.save(function (err) {
-//   if (err) console.error(err);
-//   else console.log("Anthony Book");
-// });
-// brianBook.save(function (err) {
-//   if (err) console.error(err);
-//   else console.log("Brian Book");
-// });
-// harveyBook.save(function (err) {
-//   if (err) console.error(err);
-//   else console.log("Harvey Book");
-// });
 app.post("/books", async (request, response) => {
   try {
     const bookInfo = request.body;
@@ -78,16 +44,26 @@ app.delete("/books/:id/:email", async (request, response) => {
 });
 
 app.get("/books/", async (request, response) => {
-  const books = await Book.find({});
-  response.send(books);
+ console.log(request.query.email)
+  try{
+    const userBook ={}
+    if(request.query.email){
+      userBook.email = request.query.email;
+      
+    }
+    const books = await Book.find({userBook});
+    console.log(userBook)
+    response.send(books);
+  }
+  catch(error){
+    console.log(`can't get yo books`)
+  }
+  
 });
 
 app.put("/books/:id", async (request, response) => {
   const id = request.params.id;
   const email = request.query.email;
-  console.log(email);
-  console.log(id);
-  console.log(request.query);
   try {
     const bookToUpdate = await Book.findOne({ _id: id });
     if (!bookToUpdate) {
